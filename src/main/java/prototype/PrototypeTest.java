410d46e4cbf7cdf5d143c5cc4783223f3e23b174
saurav <sauravpd58@gmail.com>
2018-08-16 02:17:56 +0530

    added prototype test class

---
+++ src/main/java/prototype/PrototypeTest.java
@@ -0,0 +1,19 @@
+package prototype;
+
+import java.util.List;
+
+public class PrototypeTest 
+{
+	public static void main(String[] args) 
+	{
+		Student student1=new Student();
+		student1.addStudent();
+		
+		Student student2=(Student) student1.clone();
+		List<String> student2List=student2.getStudentList();
+		student2List.add("Steve");
+		
+		System.out.println("Student 1 details : "+student1.getStudentList());
+		System.out.println("Student 2 details : "+student2.getStudentList());
+	}
+}
