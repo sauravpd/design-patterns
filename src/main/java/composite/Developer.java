c7ef8cfb3564c6069406f20d8d9e0127aa5bb41c
saurav <sauravpd58@gmail.com>
2018-08-17 01:26:59 +0530

    added developer class

---
+++ src/main/java/composite/Developer.java
@@ -0,0 +1,27 @@
+package composite;
+/**
+ * leaf 
+ * @author saurav
+ *
+ */
+public class Developer implements Employee
+{
+	private String name;
+	private int empId;
+	private String position;
+
+	public Developer(String name, int empId, String position) 
+	{
+		super();
+		this.name = name;
+		this.empId = empId;
+		this.position = position;
+	}
+
+	@Override
+	public void showEmployeeDetails() 
+	{
+		System.out.println(empId+" : "+name+" : "+position);
+	}
+
+}
