1acb99916de35a6a5395c7dd455f1ab8c62a0900
saurav <sauravpd58@gmail.com>
2018-08-17 01:26:33 +0530

    added company class

---
+++ src/main/java/composite/Company.java
@@ -0,0 +1,34 @@
+package composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Composite
+ * @author saurav
+ *
+ */
+public class Company implements Employee
+{
+
+	List<Employee> empList=new ArrayList<>();
+	
+	@Override
+	public void showEmployeeDetails() 
+	{
+		for(Employee emp:empList)
+		{
+			emp.showEmployeeDetails();
+		}
+	}
+	
+	public void addEmployee(Employee e)
+	{
+		empList.add(e);
+	}
+	
+	public void removeEmployee(Employee e)
+	{
+		empList.remove(e);
+	}
+}
