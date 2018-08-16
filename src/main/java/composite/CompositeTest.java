f5289b0140d2c950626809195338f9062114dce4
saurav <sauravpd58@gmail.com>
2018-08-17 01:28:45 +0530

    added composite test class

---
+++ src/main/java/composite/CompositeTest.java
@@ -0,0 +1,23 @@
+package composite;
+
+/**
+ * Composite Test class
+ * @author saurav
+ *
+ */
+public class CompositeTest 
+{
+	public static void main(String[] args) 
+	{
+		Developer dev1=new Developer("Steve",111,"Sr.Software Developer");
+		Developer dev2=new Developer("Mike",112,"Software Developer");
+		Manager mngr1=new Manager("John",222, "Project Manager");
+		
+		Company company=new Company();
+		company.addEmployee(dev1);
+		company.addEmployee(dev2);
+		company.addEmployee(mngr1);
+
+		company.showEmployeeDetails();
+	}
+}
