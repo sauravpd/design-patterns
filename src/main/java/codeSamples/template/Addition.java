94abb368946e6e2b7efdf48706f65dc419c07d10
saurav <sauravpd58@gmail.com>
2018-08-20 02:31:21 +0530

    added Addition class

---
+++ src/main/java/codeSamples/template/Addition.java
@@ -0,0 +1,24 @@
+package codeSamples.template;
+
+public class Addition extends Calculator
+{
+	private int a;
+	private int b;
+	@Override
+	void setA() 
+	{
+		a=10;
+	}
+
+	@Override
+	void setB() 
+	{
+		b=10;
+	}
+
+	@Override
+	void calculate() 
+	{
+		System.out.println("Addition is : "+(a+b));
+	}
+}
